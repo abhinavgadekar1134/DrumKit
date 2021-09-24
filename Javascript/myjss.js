88a4249aa13f178ee635dfab30d134c306ce947a
abhinavgadekar1134 <90197382+abhinavgadekar1134@users.noreply.github.com>
2021-09-24 19:20:41 +0530

    Drum

---
+++ Javascript/myjss.js
@@ -0,0 +1,16 @@
+function calldhol() {
+    var dhol = new Audio('Javascript/tom.mp3');
+    dhol.play();
+}
+function callguitar() {
+    var guiter = new Audio('Javascript/tasha.mp3');
+    guiter.play();
+}
+function calltasha() {
+    var tasha = new Audio('Javascript/openhat.mp3');
+    tasha.play();
+}
+function calldrum2() {
+    var drum2 = new Audio('Javascript/tom-4.mp3');
+    drum2.play()
+}
